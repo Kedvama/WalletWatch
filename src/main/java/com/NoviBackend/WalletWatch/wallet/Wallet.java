5f94535c6f9a875078d0038112e5a8864213233f
Kedvama <maartenvandekemp@hotmail.com>
2023-08-17 14:56:40 +0200

    added the following dependencies to the pom xml: >postgresql spring-boot-starter-data-jpa spring-boot-starter-validation These dependencies will support the communication with the reposetory and postgresql database. also created the wallet entity

--- src/main/java/com/NoviBackend/WalletWatch/wallet/Wallet.java
+++ src/main/java/com/NoviBackend/WalletWatch/wallet/Wallet.java
@@ -1,2 +1,24 @@
-package com.NoviBackend.WalletWatch.wallet;public class Wallet {
+package com.NoviBackend.WalletWatch.wallet;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "Wallets")
+public class Wallet {
+
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Constructor
+    public Wallet() {
+    }
+
+    // Getters
+    public Long getId(){
+        return id;
+    }
 }
