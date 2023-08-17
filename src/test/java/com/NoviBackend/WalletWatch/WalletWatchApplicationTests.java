6d6c5a7a9587165048130df3b5be71faa7830f86
Kedvama <maartenvandekemp@hotmail.com>
2023-08-17 13:20:05 +0200

    start of the WalletWatch project

---
+++ src/test/java/com/NoviBackend/WalletWatch/WalletWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoviBackend.WalletWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
