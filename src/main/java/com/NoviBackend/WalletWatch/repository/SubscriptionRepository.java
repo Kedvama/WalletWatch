c447472f450371108dbb2e3830c7bd322e917ca0
Kedvama <maartenvandekemp@hotmail.com>
2023-08-18 09:33:10 +0200

    forgot the @service in the subscription repo

--- src/main/java/com/NoviBackend/WalletWatch/repository/SubscriptionRepository.java
+++ src/main/java/com/NoviBackend/WalletWatch/repository/SubscriptionRepository.java
@@ -2,6 +2,8 @@ package com.NoviBackend.WalletWatch.repository;
 
 import com.NoviBackend.WalletWatch.stock.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface SubscriptionRepository extends JpaRepository<Stock, Long> {
 }
