d8e84032ae7bcf86df1361422f3ba2d647139ec3
Kedvama <maartenvandekemp@hotmail.com>
2023-08-18 09:21:39 +0200

    StockService: created the findById function, which finds a specific stock and returns it

--- src/main/java/com/NoviBackend/WalletWatch/stock/StockService.java
+++ src/main/java/com/NoviBackend/WalletWatch/stock/StockService.java
@@ -1,2 +1,24 @@
-package com.NoviBackend.WalletWatch.stock;public class StockService {
+package com.NoviBackend.WalletWatch.stock;
+
+import com.NoviBackend.WalletWatch.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class StockService {
+
+    private StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository){
+        this.stockRepository = stockRepository;
+    }
+
+    public Stock findById(int id) {
+        Optional<Stock> stock = stockRepository.findById((long) id);
+        if(stock.isEmpty())
+            return null;
+
+        return stock.get();
+    }
 }
