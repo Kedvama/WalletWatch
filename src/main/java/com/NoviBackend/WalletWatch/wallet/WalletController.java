df0fcfc7ea24e2feba388c2fe53c32613567be98
Kedvama <maartenvandekemp@hotmail.com>
2023-08-18 16:19:45 +0200

    created exception package. created: - CustomizedResponseEntity: handels the exceptions for all controllers; - ExceptionResponse: this is the body of the exceptions; - EntityNotFoundException: specific handlers for when an entity can not be found.

--- src/main/java/com/NoviBackend/WalletWatch/wallet/WalletController.java
+++ src/main/java/com/NoviBackend/WalletWatch/wallet/WalletController.java
@@ -1,5 +1,6 @@
 package com.NoviBackend.WalletWatch.wallet;
 
+import com.NoviBackend.WalletWatch.exception.EntityNotFoundException;
 import com.NoviBackend.WalletWatch.wallet.dto.WalletDto;
 import com.NoviBackend.WalletWatch.stock.StockService;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,7 @@ public class WalletController {
     public List<WalletDto> getAllPublicWallets(){
         List<WalletDto> walletList = walletService.getAllPublicWallets();
         if(walletList == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new EntityNotFoundException("No shared wallets found.");
 
         return walletList;
     }
@@ -32,7 +33,7 @@ public class WalletController {
     public Wallet getPublicWalletById(@PathVariable int id){
         Wallet wallet = walletService.findPublicById(id);
         if(wallet == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new EntityNotFoundException("Shared wallet id: " + id + ", not found.");
 
         return wallet;
     }
