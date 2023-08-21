22d37407f0cf6a95efe8ac52d18830d8e9b16b04
Kedvama <maartenvandekemp@hotmail.com>
2023-08-21 12:58:38 +0200

    created usertypes created AbstractUser, RegularUser and ProfessionalUser. Created UserController, UserRepository and UserService -> those are all empty at the moment. changed Wallet, WalletController, WalletRepository, WalletService, Stock, StockService and Subscription -> the changes zijn tussen de relaties die ze met elkaar of de users hebben.

--- src/main/java/com/NoviBackend/WalletWatch/subscription/Subscription.java
+++ src/main/java/com/NoviBackend/WalletWatch/subscription/Subscription.java
@@ -1,6 +1,20 @@
 package com.NoviBackend.WalletWatch.subscription;
 
+import com.NoviBackend.WalletWatch.wallet.Wallet;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Subscription {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private int price;
+
+    @OneToOne
+    private Wallet wallet;
 }
