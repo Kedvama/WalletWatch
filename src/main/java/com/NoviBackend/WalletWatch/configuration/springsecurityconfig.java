e7506b3ac449c1337840b122e8f6109a4304d86d
Kedvama <maartenvandekemp@hotmail.com>
2023-08-28 11:09:15 +0200

    started with springsecurity

---
+++ src/main/java/com/NoviBackend/WalletWatch/configuration/springsecurityconfig.java
@@ -0,0 +1,22 @@
+package com.NoviBackend.WalletWatch.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class springsecurityconfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws
+            Exception{
+
+    }
+
+    @Bean
+    protected SecurityFilterChain filter (HttpSecurity http)throws Exception{
+
+    }
+}
