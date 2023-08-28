f9624ae4246ec72063761bcc86d66a32ff84101c
Kedvama <maartenvandekemp@hotmail.com>
2023-08-28 11:11:34 +0200

    changed typo

--- src/main/java/com/NoviBackend/WalletWatch/configuration/SpringSecurityConfig.java
+++ src/main/java/com/NoviBackend/WalletWatch/configuration/SpringSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class springsecurityconfig {
+public class SpringSecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws
