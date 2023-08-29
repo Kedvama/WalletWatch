6031e584e39ec28ac2fe690915c835846ef81ab8
Kedvama <maartenvandekemp@hotmail.com>
2023-08-29 19:59:39 +0200

    authentication check

--- src/main/java/com/NoviBackend/WalletWatch/security/SecurityUser.java
+++ src/main/java/com/NoviBackend/WalletWatch/security/SecurityUser.java
@@ -26,5 +26,35 @@ public class SecurityUser {
     @JoinColumn(name="username")
     private Set<Authority> authorities = new HashSet<>();
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
 }
